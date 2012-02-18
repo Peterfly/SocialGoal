8b1fae54cf5d4d4b0e3cf330eadc0bf3e82e78c3
Edwin Xie <flawedmatrix@gmail.com>
2012-02-18 09:58:48 -0800

    modified:   app.js

--- app.js
+++ app.js
@@ -119,7 +119,7 @@ function handleAuthData(req, res) {
 		var client_id = '369903096353188';
 		var secret = 'd555b78179720597ace237f871a820d9';
 		var redirect_uri = 'http://ec2-184-169-254-137.us-west-1.compute.amazonaws.com/';
-		var request = facebook_client.request("GET", "oauth/access_token?client_id=" + client_id + "&redirect_uri=" + redirect_uri + "&client_secret=" + secret + "&code=" + data, {"host": "graph.facebook.com"});
+		var request = facebook_client.request("GET", "/oauth/access_token?client_id=" + client_id + "&redirect_uri=" + redirect_uri + "&client_secret=" + secret + "&code=" + data, {"host": "graph.facebook.com"});
 		request.end();
 		request.on('response', function (response) {
 			response.on('data', function (chunk) {
