4a894957accbf6a776c3a7c0307eaeaf3f5cd0a8
Edwin Xie <flawedmatrix@gmail.com>
2012-02-18 03:41:30 -0800

    modified:   app.js

--- app.js
+++ app.js
@@ -33,5 +33,9 @@ app.get('/', function (req, res) {
   res.sendfile('views/index.html');
 });
 
+server.post('/', function (req, res) {
+	res.redirect('https://www.facebook.com/dialog/oauth?client_id=369903096353188&redirect_uri=http://ec2-184-169-254-137.us-west-1.compute.amazonaws.com/');
+});
+
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
