1d012e52ab7bfdfa5045b8c13276b3940cc5a88f
Edwin Xie <flawedmatrix@gmail.com>
2012-02-18 07:16:57 -0800

    modified:   app.js

--- app.js
+++ app.js
@@ -131,9 +131,9 @@ app.get('/channel.html', function (req, res) {
 
 app.get('/fb', handlePOSTData);
 
-//app.post('/', function (req, res) {
+app.post('/', function (req, res) {
 	
-//});
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
