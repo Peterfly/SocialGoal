08ce4334ca5c935a22cc7060939eb2d7e766db09
Edwin Xie <flawedmatrix@gmail.com>
2012-02-18 07:18:04 -0800

    modified:   app.js

--- app.js
+++ app.js
@@ -132,7 +132,7 @@ app.get('/channel.html', function (req, res) {
 app.get('/fb', handlePOSTData);
 
 app.post('/', function (req, res) {
-	
+	res.redirect('https://www.facebook.com/dialog/oauth?client_id=369903096353188&redirect_uri=http://ec2-184-169-254-137.us-west-1.compute.amazonaws.com/');
 });
 
 app.listen(3000);
