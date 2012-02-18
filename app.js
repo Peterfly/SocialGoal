479349eaf291abc449cf8458815d6b92fe388fa7
Peter Qian <peterqian1993@hotmail.om>
2012-02-18 05:12:16 -0800

    database complete

--- app.js
+++ app.js
@@ -5,8 +5,24 @@
 
 var express = require('express')
   , routes = require('./routes');
+var lab = 1;
+var app = module.exports = express.createServer()
+  , io = require('socket.io').listen(app);
 
-var app = module.exports = express.createServer();
+///database initialized
+var mong = require('mongoose')
+  , schema = mong.Schema
+  , ObjectId = schema.ObjectId;
+
+db = mong.connect('mongodb://136.152.36.245/my_database');
+
+var myschema = new schema({
+	userID: {type: Number}
+  , goals : {type: Array}
+  , counts: {type: Number, default: 1}
+  , buff  : Buffer
+});
+var myModel = mong.model('goalers', myschema);
 
 // Configuration
 
@@ -32,7 +48,30 @@ app.configure('production', function(){
 //app.get('/', function (req, res) {
 //  res.sendfile('views/index.html');
 //});
-app.get('/', app.get('/', routes.index));
+function loadUser(req, res, next){
+	var user = new myModel();
+	user.userID = lab;
+	user.save(function (err, x){
+		next();
+	});
+	
+}
+/*
+console.log(lab);
+myModel.findOne({userID: lab}, function (err, doc){
+	console.log("sdsfjaf");
+	doc.counts ++;
+	res.send(doc);
+	doc.save();
+});
+console.log("first step");
+console.log("good");
+*/
+
+
+app.get('/', function (req, res){
+	res.sendfile(__dirname + '/index.html');
+});
 
 app.post('/', function (req, res) {
 	res.redirect('https://www.facebook.com/dialog/oauth?client_id=369903096353188&redirect_uri=http://ec2-184-169-254-137.us-west-1.compute.amazonaws.com/');
